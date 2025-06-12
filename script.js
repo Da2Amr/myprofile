2980f6dba637e40c92ab0c5b3e6109c445ae9a81
Da2Amr <139124158+Da2Amr@users.noreply.github.com>
2025-06-12 23:43:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
+// script.js
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("Portfolio site loaded.");
-  // Tambahkan interaksi JS jika diperlukan nanti
+  console.log("Website loaded!");
 });
